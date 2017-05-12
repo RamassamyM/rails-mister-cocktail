198dd1709135d388a8ce7d6fef09f3c2526a5a9d
Michael Ramassamy <ramassamy2000@gmail.com>
2017-05-12 12:54:43 +0200

    Add figaro - Protect my secret data in config/application.yml

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/_navbar.js
+++ app/assets/javascripts/_navbar.js
@@ -1,24 +1,27 @@
 $(document).ready(function() {
-  if (document.location.pathname == "/cocktails") {
-    // $('.navbar-wagon').css('opacity', 0);
+  var isRootUrl = (window.location.pathname.toLowerCase() === "/");
+  var isCocktailIndexUrl = (window.location.pathname.toLowerCase() === "/cocktails");
+
+  if (isRootUrl || isCocktailIndexUrl) {
+    $('.navbar-wagon').css('opacity', 0);
     $('.navbar-wagon').hide();
     $(window).on("scroll", function(event) {
       var verticalPosition = $(window).scrollTop();
       if(verticalPosition  > 400) {
-        // $('.navbar-wagon').css('opacity', 1);
-        $('.navbar-wagon').show();
+        // $('.navbar-wagon').show();
+        $('.navbar-wagon').css('opacity', 1);
       }
       if(verticalPosition  <= 400) {
-        // $('.navbar-wagon').css('opacity', 0);
+        $('.navbar-wagon').css('opacity', 0);
+      }
+      if ($('.navbar-wagon').css('opacity') === "0") {
         $('.navbar-wagon').hide();
       }
+      else {
+        $('.navbar-wagon').show();
+      }
     });
-    // if ($('.navbar-wagon').css('opacity') == 0) {
-    //   $('.navbar-wagon').hide();
-    // }
-    // if ($('.navbar-wagon').css('opacity') > 0) {
-    //   $('.navbar-wagon').show();
-    // }
+
   }
 });
 
